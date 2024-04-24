eae64045aba747d4ec0c7621544ec16e378ee0f1
liaojiqichi <86237343+liaojiqichi@users.noreply.github.com>
2024-04-24 16:05:23 +0800

    Create system.py

---
+++ system.py
@@ -0,0 +1,37 @@
+import subprocess
+import time
+
+def run_nvidia_smi():
+    result = subprocess.run(['nvidia-smi', '--query-gpu=utilization.gpu,clocks.sm,clocks.max.sm,timestamp','--format=csv,noheader,nounits'], capture_output=True, text=True)
+    sm_info = result.stdout.strip().split(',')
+    timestamp = sm_info[-1]
+    gpu_utilization = sm_info[0]
+    sm_clock = sm_info[1]
+    max_sm_clock = sm_info[2]
+    sm_utilization = float(gpu_utilization)*int(sm_clock)/int(max_sm_clock)
+    return sm_utilization
+
+def training(model):
+    sm_utilization = None 
+    try:
+        start_time = time.time()
+        script_process = subprocess.Popen(['python3', 'training1.py', '--gpu', '0', '-a', model, 'cifar'])
+    
+        while time.time() - start_time < 10:
+            if script_process.poll() is not None:
+                break
+        # read other file for kv blovks, batch size and convolutional layer
+        
+        sm_utilization = run_nvidia_smi()
+    except KeyboardInterrupt:
+        print("Keyboard interrupt detected. Stopping the data collection.")
+        script_process.terminate()
+        script_process.wait()
+    return sm_utilization
+  
+if __name__ == "__main__":
+    mdoel = vgg19
+    sm_origin = run_nvidia_smi()
+    print(sm_origin)
+    sm_new = training(model)
+    print(sm_new)
