857bb2fabd88b8a02ca8b55a4023380e9afda0c9
liaojiqichi <86237343+liaojiqichi@users.noreply.github.com>
2024-05-08 13:29:21 +0800

    Update smi_print.py

--- smi_print.py
+++ smi_print.py
@@ -11,6 +11,18 @@ def run_nvidia_smi(interval, duration, script_name):
             result = subprocess.run(['nvidia-smi', '--query-gpu=utilization.gpu,utilization.memory,clocks.sm,clocks.max.sm,timestamp',
                                      '--format=csv,noheader,nounits'], capture_output=True, text=True)
 
+            # Parse the collected SM information
+            sm_info = result.stdout.strip().split(',')
+            timestamp = sm_info[-1]
+            gpu_utilization = sm_info[0]
+            memory_utilization = sm_info[1]
+            sm_clock = sm_info[2]
+            max_sm_clock = sm_info[3]
+            sm_utilization = int(gpu_utilization)*int(sm_clock)/int(max_sm_clock)
+
+            # Write the data to the CSV file
+            csv_writer.writerow([timestamp, gpu_utilization, memory_utilization, sm_clock, max_sm_clock, sm_utilization])
+
             # Print the collected SM information
             print(result.stdout)
 
@@ -29,8 +41,8 @@ def run_nvidia_smi(interval, duration, script_name):
         script_process.wait()
 
 if __name__ == "__main__":
-    interval = 2  # Interval in seconds
-    duration = 100  # Duration in seconds
+    interval = 1  # Interval in seconds
+    duration = 7200  # Duration in seconds
     script_name = "shell.py"  # Replace with the actual path to your Python script
 
     run_nvidia_smi(interval, duration, script_name)
