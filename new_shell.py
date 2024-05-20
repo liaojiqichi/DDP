c066d9c32e2abfa8f80500c3114f9e9936121cf8
liaojiqichi <86237343+liaojiqichi@users.noreply.github.com>
2024-05-20 15:24:21 +0800

    Add files via upload

--- new_shell.py
+++ new_shell.py
@@ -10,44 +10,82 @@ dataset = load_dataset("nateraw/parti-prompts")
 dataset2 = load_dataset("succinctly/midjourney-prompts")
 
 #llm=LLM(model="facebook/opt-125m")
-
+a=0
 #print(dataset)
 while True:
 
-    random_choose1=random.randint(1,1600)
+    random_choose1=random.randint(0,1600)
 
     sentence1=dataset["train"]["Prompt"][random_choose1].split()
+        
+    #length_1=len(dataset["train"]["Prompt"][random_choose1])
 
-    length_1=len(dataset["train"]["Prompt"][random_choose1])
+    #len1=random.randint(int(len(sentence1)/2),len(sentence1))
 
-#print(sentence1)
+    #part1=random.choices(sentence1,k=len1)
+   
+   # print(sentence1)
+    #print(part1)
+#SENTENCE 2
+    random_choose3=random.randint(0,1600)
 
-#print(type(dataset["train"]["Prompt"][random_choose1]))
+    sentence3=dataset["train"]["Prompt"][random_choose3].split()
 
-#print(length_1)
+    #length_3=len(dataset["train"]["Prompt"][random_choose3])
 
-#print(len(sentence1))
-    len1=random.randint(0,len(sentence1))
+    #len3=random.randint(int(len(sentence3)/2),len(sentence3))
 
-    part1=random.choices(sentence1,k=len1)
+    #part3=random.choices(sentence3,k=len3)
 
 #print(dataset2)
-
-    random_choose2=random.randint(1,12320)
+    
+    random_choose2=random.randint(0,12320)
 
     sentence2=dataset2["test"]["text"][random_choose2].split()
+    
+    #length_2=len(dataset2["test"]["text"][random_choose2])
 
-    length_2=len(dataset2["test"]["text"][random_choose2])
+    #len2=random.randint(int(len(sentence2)/2),len(sentence2))
 
-    len2=random.randint(0,len(sentence2))
+   # part2=random.choices(sentence2,k=len2)
+#sentence 4
+   # random_choose4=random.randint(0,12320)
 
-    part2=random.choices(sentence2,k=len2)
+    sentence4=dataset2["test"]["text"][random_choose2].split()
+
+  #  length_4=len(dataset2["test"]["text"][random_choose2])
+
+ #   len4=random.randint(int(len(sentence4)/2),len(sentence4))
+
+#    part4=random.choices(sentence4,k=len4)
+   
+    #prompts=' '.join(part1+part2+part3+part4+sentence2)
+   
+    prompts=' '.join(sentence1+sentence2+sentence3+sentence4)
+   # print("finish")
+   # if len(prompts)<200:
+    prompts =''.join(prompts+prompts)
+    #    continue
+    if len(prompts)<300:
+        
+        prompts =''.join(prompts+prompts)
+   
+
+    prompts =''.join(prompts+prompts)
+
+    prompts =''.join(prompts+prompts)
+    print(prompts)
+    print("\n")   
+    with open(r'/DDP/prompts.txt', 'a+') as fp:
+        fp.write("%d\n" %a)
+        fp.write("%s\n" %prompts)
+    fp.close()
 
 #print(part2)
 
-#print(sentence2)
+    #print(sentence2)
 
-    prompts=' '.join(part1+part2)
+#    prompts=' '.join(part1+part2)
 
     headers = {"Content-Type": "application/json"}
 
@@ -64,7 +102,6 @@ while True:
 
 
     res = requests.post(url, headers=headers, data=json.dumps(da))
-
-    print(res.text)
-
-    time.sleep(1)                   
+    
+    print(res.text)     
+    print('\n')
--- shell.py
+++ shell.py
@@ -4,62 +4,40 @@ import random
 import time
 import requests
 import json
-
-dataset = load_dataset("nateraw/parti-prompts")
-
-dataset2 = load_dataset("succinctly/midjourney-prompts")
-
+import re
 #llm=LLM(model="facebook/opt-125m")
 
-i=0
+file = open('/DDP/prompts.txt','r')
 
-#print(dataset)
-while i<100:
+prompts = ''
 
-    i=i+1
+Lines = file.readlines()
 
-    random_choose1=random.randint(1,1632)
+for line in Lines:
 
-    sentence1=dataset["train"]["Prompt"][random_choose1].split()
+    if len(line)==1 or len(line)==2 or len(line)==3 or len(line)==4:
+            
+            prompts = ''
+            
+            continue
+  
+    prompts=''.join(prompts+line)
 
-    length_1=len(dataset["train"]["Prompt"][random_choose1])
+    headers = {"Content-Type": "application/json"}
 
-#print(sentence1)
-
-#print(type(dataset["train"]["Prompt"][random_choose1]))
-
-#print(length_1)
-
-#print(len(sentence1))
-    len1=random.randint(0,len(sentence1))
-
-    part1=random.choices(sentence1,k=len1)
-
-#print(dataset2)
-
-    random_choose2=random.randint(1,12320)
-
-    sentence2=dataset2["test"]["text"][random_choose2].split()
-
-    length_2=len(dataset2["test"]["text"][random_choose2])
-
-    len2=random.randint(0,len(sentence2))
-
-    part2=random.choices(sentence2,k=len2)
-
-#print(part2)
-
-#print(sentence2)
-
-    prompts=' '.join(part1+part2)
-
-    url="http://0.0.0.0:8000/generate"
-
-    da={
-        "prompt":prompts
+    url="http://0.0.0.0:8000/v1/chat/completions"
+    
+    da = {
+            "model": "meta-llama/Llama-2-7b-chat-hf",
+            "messages": [
+                    {"role": "system", "content": prompts},
+                    {"role": "user", "content": prompts}
+                ],
+            "tempereture": 0
         }
-    res = requests.post(url,data=json.dumps(da))
 
-    print(res.text)
+    print(prompts)
 
-    time.sleep(1)
+    res = requests.post(url, headers=headers, data=json.dumps(da))
+
+
